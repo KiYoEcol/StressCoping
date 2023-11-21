7b32c8d017ab9af0a57d81114cc79ccbcacbe809
KiYo <kiyonori.nakashima@gmail.com>
2023-11-21 21:03:23 +0900

    rename FirstFragment and fragment_first

--- app/src/main/java/com/example/stresscoping/StressCopingFragment.kt
+++ app/src/main/java/com/example/stresscoping/StressCopingFragment.kt
@@ -6,14 +6,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
-import com.example.stresscoping.databinding.FragmentFirstBinding
+import com.example.stresscoping.databinding.FragmentStressCopingBinding
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
-class FirstFragment : Fragment() {
+class StressCopingFragment : Fragment() {
 
-    private var _binding: FragmentFirstBinding? = null
+    private var _binding: FragmentStressCopingBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -25,7 +25,7 @@ class FirstFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
 
-        _binding = FragmentFirstBinding.inflate(inflater, container, false)
+        _binding = FragmentStressCopingBinding.inflate(inflater, container, false)
         binding.buttonStressCoping.setOnClickListener { clickButtonStressCoping() }
         return binding.root
 
