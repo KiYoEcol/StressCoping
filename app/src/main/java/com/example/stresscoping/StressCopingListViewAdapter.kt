378f467a3bfd49c6a50ef70d5e8e1e8359f47eca
KiYo <kiyonori.nakashima@gmail.com>
2023-12-12 17:55:19 +0900

    item view change to binding

--- app/src/main/java/com/example/stresscoping/StressCopingListViewAdapter.kt
+++ app/src/main/java/com/example/stresscoping/StressCopingListViewAdapter.kt
@@ -4,26 +4,28 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.example.stresscoping.databinding.ItemStressCopingListBinding
 
 class StressCopingListViewAdapter(
     private val list: List<StressCopingModel>,
     private val listener: Listener?
-) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+) : RecyclerView.Adapter<StressCopingListViewAdapter.StressCopingListViewHolder>() {
     interface Listener {
         fun onClickItem(tappedView: View, stressCopingModel: StressCopingModel)
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        val itemView: View = LayoutInflater.from(parent.context)
-            .inflate(R.layout.item_stress_coping_list, parent, false)
-        return StressCopingListViewHolder(itemView)
+    class StressCopingListViewHolder(val binding: ItemStressCopingListBinding) :
+        RecyclerView.ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): StressCopingListViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val binding = ItemStressCopingListBinding.inflate(inflater, parent, false)
+        return StressCopingListViewHolder(binding)
     }
 
-    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        (holder as? StressCopingListViewHolder)?.apply {
-            textView.text = list[position].stressCoping
-            itemView.setOnClickListener { listener?.onClickItem(it, list[position]) }
-        }
+    override fun onBindViewHolder(holder: StressCopingListViewHolder, position: Int) {
+        holder.binding.textviewStressCopingItem.text = list[position].stressCoping
+        holder.binding.root.setOnClickListener { listener?.onClickItem(it, list[position]) }
     }
 
     override fun getItemCount(): Int {
