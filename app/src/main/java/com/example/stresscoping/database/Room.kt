2ad2526b65aac004f815abfabc8d90c6e74be1ed
KiYo <kiyonori.nakashima@gmail.com>
2023-12-14 21:34:55 +0900

    Add Room.kt

---
+++ app/src/main/java/com/example/stresscoping/database/Room.kt
@@ -0,0 +1,17 @@
+package com.example.stresscoping.database
+
+import android.content.Context
+import androidx.room.Room
+
+private lateinit var STRESS_COPING_DATABASE_INSTANCE: StressCopingDatabase
+fun getStressCopingDatabase(context: Context): StressCopingDatabase{
+    synchronized(StressCopingDatabase::class.java){
+        if (!::STRESS_COPING_DATABASE_INSTANCE.isInitialized){
+            STRESS_COPING_DATABASE_INSTANCE = Room.databaseBuilder(context.applicationContext,
+                StressCopingDatabase::class.java,
+                "stress_coping"
+            ).build()
+        }
+    }
+    return STRESS_COPING_DATABASE_INSTANCE
+}
