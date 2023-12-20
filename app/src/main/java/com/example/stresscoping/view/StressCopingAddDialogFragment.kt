c39942e3c6575bbd9fb1fb4b2c1e80bcdabb2806
KiYo <kiyonori.nakashima@gmail.com>
2023-12-20 19:19:52 +0900

    Add StressCopingAddDialogFragment.kt

---
+++ app/src/main/java/com/example/stresscoping/view/StressCopingAddDialogFragment.kt
@@ -0,0 +1,40 @@
+package com.example.stresscoping.view
+
+import android.app.Dialog
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.widget.AppCompatEditText
+import androidx.fragment.app.DialogFragment
+import com.example.stresscoping.R
+
+class StressCopingAddDialogFragment : DialogFragment() {
+
+    interface Listener {
+        fun onClickOkOnAddDialog(title: String)
+    }
+
+    var listener: Listener? = null
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        //return super.onCreateDialog(savedInstanceState)
+        return activity?.let {
+            val editText = AppCompatEditText(it)
+            val builder = AlertDialog.Builder(it)
+            builder.setTitle(R.string.title_stress_coping_add_dialog)
+                .setMessage(R.string.msg_stress_coping_add_dialog)
+                .setView(editText)
+                .setPositiveButton(R.string.btn_ok) { dialog, _ ->
+                    // OKボタンを押したときの処理
+                    val title = editText.text.toString()
+                    Log.d("TEST","テスト parentFragment is Listener? ${parentFragment is Listener}")
+                    listener?.onClickOkOnAddDialog(title)
+
+                    dialog.dismiss()
+                }
+                .setNegativeButton(R.string.btn_cancel) { _, _ -> }
+            // Create the AlertDialog object and return it
+            builder.create()
+        } ?: throw IllegalStateException("Activity cannot be null")
+    }
+}
