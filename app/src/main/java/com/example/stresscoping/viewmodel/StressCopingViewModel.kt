be1ed01db24f1567c108188fce30206d802306af
KiYo <kiyonori.nakashima@gmail.com>
2023-12-22 21:24:16 +0900

    MutableLiveData of textStressCoping change to LiveData

--- app/src/main/java/com/example/stresscoping/viewmodel/StressCopingViewModel.kt
+++ app/src/main/java/com/example/stresscoping/viewmodel/StressCopingViewModel.kt
@@ -1,7 +1,6 @@
 package com.example.stresscoping.viewmodel
 
 import android.app.Application
-import android.util.Log
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
@@ -15,21 +14,20 @@ class StressCopingViewModel(application: Application) : AndroidViewModel(applica
     private var repository: StressCopingRepository =
         StressCopingRepository(getStressCopingDatabase(application))
     val stressCopings: LiveData<List<StressCopingModel>> = repository.allFlow.asLiveData()
-    val textStressCoping: MutableLiveData<String> = MutableLiveData()
+    private val _textStressCoping = MutableLiveData<String>()
+    val textStressCoping: LiveData<String> = _textStressCoping
 
     init {
-        textStressCoping.value = application.getString(R.string.first_text_on_stress_coping)
+        _textStressCoping.value = application.getString(R.string.first_text_on_stress_coping)
     }
 
     fun clickChoose() {
         val stressCopings = this@StressCopingViewModel.stressCopings.value
         if (!stressCopings.isNullOrEmpty()) {
             val stressCoping = stressCopings.random()
-            textStressCoping.postValue(stressCoping.title)
-            Log.d("TEST", "テスト $stressCoping")
+            _textStressCoping.postValue(stressCoping.title)
         } else {
-            textStressCoping.postValue(getApplication<Application>().getString(R.string.empty_text_on_stress_coping))
-            Log.d("TEST", "stressCopings is null or empty $stressCopings")
+            _textStressCoping.postValue(getApplication<Application>().getString(R.string.empty_text_on_stress_coping))
         }
     }
 }
